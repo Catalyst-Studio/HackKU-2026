ec3160eeab6986a608167448b470e65f174b81b1
Lawrence W. Burton III <lawrencewburtoniii@gmail.com>
2026-04-18 10:42:15 -0500

    add user creation

--- user.py
+++ user.py
@@ -1,4 +1,10 @@
-
+import base64
+import hashlib
+import uuid
+from os import urandom
+from crud import database
+import bcrypt
+from pydantic import BaseModel
 
 
 def get_user(user_id: str):
@@ -9,5 +15,35 @@ def validate_user(email: str, password: str):
     return True
 
 
-class User:
-    pass
+def encrypt_password(password: str, salt_raw: str):
+    salt = base64.b64decode(salt_raw.encode())
+    utf_8_password = password.encode("utf-8")
+    hashed_password = bcrypt.hashpw(utf_8_password, salt)
+    hashed_password_string = base64.b64encode(hashed_password).decode()
+    return hashed_password_string
+
+
+def create_user(name: str, email: str, password: str):
+    userid = str(uuid.uuid4())
+    salt = bcrypt.gensalt()
+    salt_string = base64.b64encode(salt).decode()
+    encrypted_password = encrypt_password(password, salt_string)
+    user = User(
+        userID=userid,
+        name=name,
+        password=encrypted_password,
+        salt=salt_string,
+        start_of_week="Monday",
+        email=email
+    )
+    database["Users"].insert_one(user.model_dump())
+
+
+
+class User(BaseModel):
+    userID: str
+    name: str
+    password: str
+    salt: str
+    start_of_week: str
+    email: str
