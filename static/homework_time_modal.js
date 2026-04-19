eb0fd504569a876468e50ad84cf055ac0b6c67b0
soph1428 <sophiagroth16@gmail.com>
2026-04-19 01:49:43 -0500

    added homework availability modal and script

--- static/homework_time_modal.js
+++ static/homework_time_modal.js
@@ -1,26 +1,29 @@
-// let modal;
 // window.addEventListener('load', (event) => {
-//     modal = bootstrap.Modal.getOrCreateInstance("#homework-availability-modal");
+//     let modal = bootstrap.Modal.getOrCreateInstance("#homework-availability-modal");
 //     modal.show();
 // });
 
 function addTimeAvailability(elem) {
     let div = document.createElement("div");
     div.className = "d-flex";
-    let count = "-" + elem.parentElement.parentElement.childElementCount;
-
+    let count = elem.parentElement.parentElement.childElementCount;
     let newInput1 = elem.parentElement.querySelector("[id*='Time1']").cloneNode();
     newInput1.value = "";
     let newInput2 = elem.parentElement.querySelector("[id*='Time2']").cloneNode();
     newInput2.value = "";
+
+    newInput1.id = newInput1.id.replace(newInput1.id.substring(9), count);
+    newInput1.name = newInput1.id
+
+    newInput2.id = newInput2.id.replace(newInput2.id.substring(9), count);
+    newInput2.name = newInput2.id;
+
     let day = newInput1.id.substring(0, 3);
 
     div.id = day + "Flex" + "-" + Array.from(elem.parentElement.parentElement.children).filter(elem => elem.id.includes(day + "Flex")).length;
-    newInput1.ariaLabel = day.substring(0, 1).toUpperCase() + day.substring(1) + "day Time 1" + count;
-    newInput1.id = newInput1.id += count;
 
+    newInput1.ariaLabel = day.substring(0, 1).toUpperCase() + day.substring(1) + "day Time 1" + count;
     newInput2.ariaLabel = day.substring(0, 1).toUpperCase() + day.substring(1) + "day Time 2" + count;
-    newInput2.id = newInput2.id += count;
 
     div.appendChild(newInput1, elem.parentElement.children.length - 1);
     let lineBreak = document.createElement("p");
@@ -31,7 +34,9 @@ function addTimeAvailability(elem) {
     let removeButton = document.createElement("button");
     removeButton.className = "btn btn-danger m-2 flex-fill";
     removeButton.innerHTML = "<i class='iconoir-minus fs-5'>"
-    removeButton.onclick = function() {elem.parentElement.parentElement.removeChild(div)}
+    removeButton.onclick = function () {
+        elem.parentElement.parentElement.removeChild(div)
+    }
     div.appendChild(removeButton);
     elem.parentElement.parentElement.insertBefore(div, elem.parentElement.parentElement[elem.parentElement.parentElement.childElementCount - 1]);
 }
@@ -52,6 +57,9 @@ function checkTime(elem) {
     }
 }
 
-function sendHomeworkAvailabilityTimes() {
-    if (document.querySelector("form").isValid) modal.hide();
+function addHomeworkAvailabilityTimes() {
+    if (document.querySelector("form").isValid) {
+        modal.hide();
+        //add to server
+    }
 }
